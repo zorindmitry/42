1e9bb7b68cbb1b7f570cb61218e495f93eb7825d
Dmytro ZORIN <dzorin@e2r8p6.unit.ua>
2017-11-10 17:57:46 +0200

    add bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,12 +1,21 @@
-#include "libft"
+#include "libft.h"
 #include <stdio.h>
-#include <
+#include <string.h>
 void ft_bzero(void *s, size_t n)
 {
-
+	while(n > 0)
+	{
+		int i = 0;
+		((unsigned char *)s)[i++] = '\0';
+		n--;
+	}
 }
-
-int main()
-{
-	char c[10] = "qwertyu";
-}
+	int main()
+	{
+		char c[10] = "005esyu";
+		printf("start:%s",c);
+		printf("\n");
+		ft_bzero(c+3, 0);
+		printf("start:%s",c);
+		printf("\n");
+	}
