148d68ba3d396ef7c800810b4a9f792fdf58e182
Dmytro ZORIN <dzorin@e1r7p9.unit.ua>
2017-11-15 17:53:03 +0200

    reset changes

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzorin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/15 17:52:02 by dzorin            #+#    #+#             */
+/*   Updated: 2017/11/15 17:52:06 by dzorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include <stdio.h>
 #include <string.h>
@@ -15,7 +27,7 @@ void ft_bzero(void *s, size_t n)
 		char c[10] = "005esyu";
 		printf("start:%s",c);
 		printf("\n");
-		ft_bzero(c+3, 0);
+		ft_bzero(c+3, 1);
 		printf("start:%s",c);
 		printf("\n");
 	}
