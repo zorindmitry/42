8e494b23030c210948cbb48357147a3699c00670
Dmytro ZORIN <dzorin@e2r5p7.unit.ua>
2017-11-21 16:54:40 +0200

    add strlen change libft

--- libft.h
+++ libft.h
@@ -9,4 +9,5 @@ void	*	ft_memcpy(void * dst, const void * src, size_t n);
 void	*	ft_memmove(void *s1, const void *s2, size_t n);
 void	*	memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t	ft_strlen(const char *s);
 #endif
