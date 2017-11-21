8a205f8f1eaa2b5325da3180621aee9c83b7c6af
Dmytro ZORIN <dzorin@e2r5p7.unit.ua>
2017-11-21 16:38:41 +0200

    add ft_memcmp change libft

---
+++ ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzorin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/21 16:18:47 by dzorin            #+#    #+#             */
+/*   Updated: 2017/11/21 16:18:52 by dzorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	int		i;
+	int		result;
+	char	*sc1;
+	char	*sc2;
+
+	i = 0;
+	result = 0;
+	sc1 = (char *)s1;
+	sc2 = (char *)s2;
+	while (i < n)
+	{
+		if (sc1[i] > sc2[i] || sc1[i] < sc2[i])
+			return (sc1[i] - sc2[i]);
+		i++;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -8,5 +8,5 @@ void	*	ft_memcpy(void * dst, const void * src, size_t n);
 
 void	*	ft_memmove(void *s1, const void *s2, size_t n);
 void	*	memchr(const void *s, int c, size_t n);
-
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
