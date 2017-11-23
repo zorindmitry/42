4da5089af38bafaaceeb33511944cee7b5f40494
Dmytro ZORIN <dzorin@e1r7p10.unit.ua>
2017-11-23 16:08:29 +0200

    last 4 func

--- libft.h
+++ libft.h
@@ -31,5 +31,9 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
-
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c)
+int		ft_toupper(int c)
 #endif
--- untitled.c
+++ untitled.c
@@ -171,3 +171,26 @@ int main()
 	}
 	return 0;
 }
+
+///////ft_isalnum//////ft_isascii/////
+#include <ctype.h>
+int main()
+{
+	int i = 0;
+	char c[99] = "Ca+1w*9(";
+	while (c[i] != 0)
+	{
+		printf("%d\n",isalnum(c[i]));
+		i++;
+	}
+	i = 0;
+	printf("\n");
+	char c1[99] = "Ca+1w*9(";
+	while (c1[i] != 0)
+	{
+		printf("%d\n", ft_isalnum(c1[i]));
+		i++;
+	}
+	return 0;
+}
+
