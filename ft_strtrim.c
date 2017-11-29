6aaecdd4e5a35844555a35a2f2ecbfb7ed6112fb
Dmytro ZORIN <dzorin@e1r7p8.unit.ua>
2017-11-29 19:18:35 +0200

    work with strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -17,29 +17,68 @@ char	*ft_strtrim(char const *s)
 	char *res;
 	int len;
 	int i;
-
-	i = 0;
-	len = ft_strlen(c);
+	int j;
 	if (!s)
 		return (NULL);
+	j = 0;
+	i = 0;
+	len = ft_strlen(s);
 	while (s[len - 1] == ' ' || s[len - 1] == '\t' || s[len - 1] == '\n')
 		len--;
 	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
-	{ 
-		i++;
+	{
 		len--;
+		i++;
 	}
-	res = (char *)malloc(sizeof(char) * (len + 1));
-	if (!res)
+	if (len <= 0)
+		len = 0;
+	if(!(res = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	while (j < len)
+	s = s + i;
+	while (s[i] != '\0' && j < len)
 	{
 		res[j] = s[i];
+		j++;
+		i++;
 	}
+	res[j] = '\0';
+	return (res);
+}
 
-int		main(void)
+int					main(void)
 {
-	char a[] = "   Hello   world   ";
-	printf("%s\n", ft_strtrim(a));
-	return 0;
+	char			str[] = "  \t    \t\nBon\t \njour\t\n  \n     ";
+	char			str2[] = "Bonjour";
+	char			str3[] = "  \t\t\t   ";
+	char			*r;
+
+	//ft_strtrim(NULL);
+	r = ft_strtrim(str);
+	if (strcmp(r, "Bon\t \njour"))
+	{
+		printf("Error Line %d, Funct %s : \n\033[31mft_strstrim(\"  \t    \t\nBon\t \njour\t\n  \n     \").\nExpected ret = \"Bon\t \njour\" \
+			   but have ret = \"%s\"\033[0m\n", __LINE__ - 2, __func__, r);
+		free(r);
+		return (0);
+	}
+	free(r);
+	r = ft_strtrim(str2);
+    if (strcmp(r, "Bonjour") || (str2 == r))
+    {
+        printf("Error Line %d, Funct %s : \n\033[31mft_strstrim(\"Bonjour\").\nExpected ret = \"Bonjour\" and differents pointers\
+			   but have ret = \"%s\" and our: %p / your: %p\033[0m\n", __LINE__ - 2, __func__, r, str2, r);
+		free(r);
+        return (0);
+    }
+    free(r);
+	r = ft_strtrim(str3);
+    if (strcmp(r, "") || (str3 == r))
+    {
+        printf("Error Line %d, Funct %s : \n\033[31mft_strstrim(\"  \t\t\t  \").\nExpected ret = \"\" and different\
+s pointers but have ret = \"%s\" and our: %p / your: %p\033[0m\n", __LINE__ - 2, __func__, r, str3, r);
+		free(r);
+        return (0);
+    }
+    free(r);
+	return (1);
 }
